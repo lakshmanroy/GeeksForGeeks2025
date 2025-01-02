70fbee5e9ed3a9ff4c8174700d4406ba90c5821b
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-02 17:47:33 +0530

    commit

--- ProblemList/StockAndBuy.java
+++ ProblemList/StockAndBuy.java
@@ -4,7 +4,6 @@ public class StockAndBuy {
         for(int i=1;i<arr.length;i++){
             if(arr[i]>arr[i-1]){
                 maxProfit+=arr[i]-arr[i-1];
-
             }
         }
         return maxProfit;
