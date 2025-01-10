2631bd729112a8ddef61baef214516d7ac095535
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-10 22:01:20 +0530

    commit

---
+++ ProblemList/ArraysSearch.java
@@ -0,0 +1,16 @@
+public class ArraysSearch {
+    public static int search(int arr[],int x){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={1, 2, 3, 4};
+        int x=3;
+
+        System.out.println(search(arr, x));
+    }
+}
