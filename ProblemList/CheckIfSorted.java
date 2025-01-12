5d61111f0cf3fe2a7d92ac702aef4b9a3df018df
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-12 23:32:42 +0530

    commit

---
+++ ProblemList/CheckIfSorted.java
@@ -0,0 +1,16 @@
+public class CheckIfSorted {
+    public static boolean isSorted(int arr[]){
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr[]={10, 20, 30, 40, 50};
+
+        System.out.println(isSorted(arr));
+    }
+}
