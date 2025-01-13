5d2d1ce3f420ad70103a5c0203fe348ab72f761f
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-13 21:55:56 +0530

    commit

---
+++ ProblemList/SquarRoot.java
@@ -0,0 +1,25 @@
+public class SquarRoot {
+public static int floorStr(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    int start=1,end=n,result=0;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(mid<=n/mid){
+            result=mid;
+            start=mid+1;
+
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return result;
+}
+    public static void main(String[] args) {
+        int n=4;
+
+        System.out.println(floorStr(n));
+    }
+}
