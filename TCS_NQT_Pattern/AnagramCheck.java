052ac6f1852bea57be9133674a28fd22a1d46a86
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-16 18:46:51 +0530

    commit

---
+++ TCS_NQT_Pattern/AnagramCheck.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean isCheck(String s,String t){
+       if(s.length()!=t.length()){
+        return false;
+       }
+       char sArray[]=s.toCharArray();
+       char tArray[]=t.toCharArray();
+
+       Arrays.sort(sArray);
+       Arrays.sort(tArray);
+
+       return Arrays.equals(sArray,tArray);
+
+    }
+    public static void main(String[] args) {
+        String s="listen";
+        String t="silent";
+
+        System.out.println(isCheck(s, t));
+    }
+}
