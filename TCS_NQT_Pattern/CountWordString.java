98c7b9deeaf518c565ccd80c389d181fcf59309c
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-17 20:41:43 +0530

    commit

---
+++ TCS_NQT_Pattern/CountWordString.java
@@ -0,0 +1,14 @@
+public class CountWordString {
+    public static int countWord(String str){
+        if(str==null || str.trim().isEmpty()){
+            return 0;
+        }
+        return str.trim().split(" +").length;
+    }
+    public static void main(String[] args) {
+        String str="Hello World! Welcome to TCS";
+
+        System.out.println(countWord(str));
+    }
+}
+
