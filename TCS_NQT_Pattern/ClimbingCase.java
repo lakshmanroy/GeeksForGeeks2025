0b67b2559c1930e8e037d3b20d237591787f8072
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-02-03 16:29:53 +0530

    commit

---
+++ TCS_NQT_Pattern/ClimbingCase.java
@@ -0,0 +1,20 @@
+public class ClimbingCase {
+    public static int stairCase(int n){
+        if(n<=1){
+            return 1;
+        }
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String args[]){
+        int n=4;
+
+        System.out.print(stairCase(n));
+    }
+}
