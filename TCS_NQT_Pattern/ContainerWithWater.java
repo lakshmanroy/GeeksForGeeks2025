d6424712349829e79cc6c3a2e16040acc3d02e48
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-02-03 11:43:32 +0530

    commit

---
+++ TCS_NQT_Pattern/ContainerWithWater.java
@@ -0,0 +1,23 @@
+public class ContainerWithWater {
+    public static int mostWater(int height[]){
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+        while(left<right){
+            int currArea=Math.min(height[left],height[right])*(right-left);
+            maxArea=Math.max(maxArea,currArea);
+            if(height[left]<height[right]){
+               left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+    public static void main(String args[]){
+        int height[]={1,8,6,2,5,4,8,3,7};
+
+        System.out.println(mostWater(height));
+    }
+}
