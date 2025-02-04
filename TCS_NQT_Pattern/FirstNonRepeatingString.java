b1ac9fe066499384dacce3231fa33395616e5c6b
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-02-04 10:07:23 +0530

    commit

---
+++ TCS_NQT_Pattern/FirstNonRepeatingString.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class FirstNonRepeatingString {
+    public static char NonRepeating(String s){
+        HashMap<Character,Integer>map=new HashMap<>();
+        
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:s.toCharArray()){
+            if(map.get(c)==1){
+                return c;
+            }
+        }
+        return '\0';
+    }
+    public static void main(String args[]){
+        String s="swisi";
+
+        System.out.print(NonRepeating(s));
+    }
+}
